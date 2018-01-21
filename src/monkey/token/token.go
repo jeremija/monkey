ef95a6749331caa9a7896599778104dfc68758e3
Jerko Steiner <jerko.steiner@nyumc.org>
2018-01-21 11:05:24 -0500

    Add more one-letter tokens

--- src/monkey/token/token.go
+++ src/monkey/token/token.go
@@ -16,8 +16,16 @@ const (
 	INT   = "INT"
 
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS		 = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
 
 	// Delimiters
 	COMMA     = ","
